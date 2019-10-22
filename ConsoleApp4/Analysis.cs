edd9d2c91712a17a4e5cebbba9869581ec641dd7
Lera2701 <valeriia.hlotka@nure.ua>
2019-10-22 19:03:27 +0300

    Add files via upload

---
+++ ConsoleApp4/Analysis.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppTh
+{
+    class Analysis
+    {
+        public static string flag;
+        public static void FindFormula()
+        {
+            var question = ("Повторяются ли элементы в выборке? [+/-]",
+                "Важен ли порядок элементов? [+/-]", "Все ли элементы входят в выборку? [+/-]");
+
+            Console.WriteLine($"{question.Item1}");
+            
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"{question.Item2}");
+                    switch (Console.ReadLine())
+                    {
+                        case "+":
+                            Console.WriteLine($"{question.Item3}");
+                            switch (Console.ReadLine())
+                            {
+                                case "+":
+                                    Console.WriteLine("Это перестановки с повторением!");
+                                    flag = "pp";
+                                    Console.ReadKey();
+                                    break;
+                                case "-":
+                                    Console.WriteLine("Это размещения с повторением!");
+                                    flag = "aa";
+                                    Console.ReadKey();
+                                    break;
+                                default: goto case "+";
+                            }
+                            break;
+                        case "-":
+                            Console.WriteLine("Это комбинации с повторением!");
+                            flag = "cc";
+                            Console.ReadKey();
+                            break;
+                        default: goto case "+";
+                    }
+                    break;
+
+                case "-":
+                    Console.WriteLine($"{question.Item2}");
+                    switch (Console.ReadLine())
+                    {
+                        case "+":
+                            Console.WriteLine($"{question.Item3}");
+                            switch (Console.ReadLine())
+                            {
+                                case "+":
+                                    Console.WriteLine("Это перестановки!");
+                                    flag = "p";
+                                    Console.ReadKey();
+                                    break;
+                                case "-":
+                                    Console.WriteLine("Это размещения!");
+                                    flag = "a";
+                                    Console.ReadKey();
+                                    break;
+                                default: goto case "+";
+                            }
+                            break;
+                        case "-":
+                            Console.WriteLine("Это комбинации!");
+                            flag = "c";
+                            Console.ReadKey();
+                            break;
+                        default: goto case "+";
+                    }
+                    break;
+
+                default: goto case "+";
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Computing.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppTh
+{
+    class Computing
+    {
+        public static void Calculate()
+        {
+            Console.WriteLine("Введите, из скольких (для перестановок с повторением - общее кол-во элементов):");
+
+            int num = ManageInputCorrectness();
+            
+            switch (Analysis.flag)
+            {
+                case "pp":
+                    Console.WriteLine("Введите количество различных элементов ряда:");
+                    int j = ManageInputCorrectness();
+                    Console.WriteLine("Введите по порядку, сколько раз повторяется в ряду каждый элемент:");
+                    var denominator = 1;
+                    for (var i = 0; i < j; i++)
+                    {
+                        int input = ManageInputCorrectness();
+                        denominator *= Factorial(input);
+                    }
+                    Console.WriteLine($"Результат: {Factorial(num) / denominator}");
+                    Console.ReadKey();
+                    break;
+                case "p":
+                    Console.WriteLine($"Результат: {Factorial(num)}");
+                    Console.ReadKey();
+                    break;
+                case "aa":
+                    Console.WriteLine("Введите, по сколько:");
+                    var k = ManageInputCorrectness();
+                    if (k >= num) goto case "aa";
+                    Console.WriteLine($"Результат: {Math.Pow(num, k)}");
+                    Console.ReadKey();
+                    break;
+                case "a":
+                    Console.WriteLine("Введите, по сколько:");
+                    k = ManageInputCorrectness();
+                    if (k >= num) goto case "a";
+                    Console.WriteLine($"Результат: {Factorial(num) / Factorial(num - k)}");
+                    Console.ReadKey();
+                    break;
+                case "cc":
+                    Console.WriteLine("Введите, по сколько:");
+                    k = ManageInputCorrectness();
+                    if (k >= num) goto case "cc";
+                    Console.WriteLine($"Равносильно комбинациям из {k} по {num + k - 1}");
+                    Console.WriteLine($"Результат: {Factorial(num + k - 1) / (Factorial(num - 1) * Factorial(k))}");
+                    Console.ReadKey();
+                    break;
+                case "c":
+                    Console.WriteLine("Введите, по сколько:");
+                    k = ManageInputCorrectness();
+                    if (k >= num) goto case "c";
+                    Console.WriteLine($"Результат: {Factorial(num) / (Factorial(num - k) * Factorial(k))}");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
+        public static int Factorial(int x)
+        {
+            if (x == 1 || x == 0) return 1;
+            return x * Factorial(x - 1);
+        }
+
+        public static int ManageInputCorrectness()
+        {
+            string input = Console.ReadLine();
+            if (Int32.TryParse(input, out int num))
+            {
+                if (num > 0)
+                return Convert.ToInt32(input);
+            }
+
+            Console.WriteLine("Введите корректное число");
+            return ManageInputCorrectness();
+        }
+
+        public static void IsSolvingNeeded()
+        {
+            Console.WriteLine("Решим задачку? [+/-]");
+            var p = Console.ReadLine();
+            if (p == "+" || string.IsNullOrEmpty(p))
+            {
+                Analysis.FindFormula();
+                Calculate();
+            }
+
+            else if (p == "-") return;
+
+            else IsSolvingNeeded();
+        }
+            
+    }
+    
+}
+
